60a862e1e68c86d72e2f991f3727fe497607b775
Jeff Anderson <j3ff.anderson@gmail.com>
2019-12-10 01:46:44 -0800

    testing reset in p.cpp

--- Piezas.cpp
+++ Piezas.cpp
@@ -39,12 +39,11 @@ Piezas::Piezas()
 void Piezas::reset()
 {
 
-//  board.resize(3);
-//  for (int i = 0; i < BOARD_ROWS; i++){
-//    for (int j = 0; j < BOARD_COLS; j++){
-//      board[i][j] = Blank;
-//    }
-//  }
+  for (int i = 0; i < BOARD_ROWS; i++){
+    for (int j = 0; j < BOARD_COLS; j++){
+      board[i][j] = Blank;
+    }
+  }
   turn = X;
 }
 
