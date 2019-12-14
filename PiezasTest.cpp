ce375eeb1fccb8fd00f1211a795621f07c43b545
Jeff Anderson <j3ff.anderson@gmail.com>
2019-12-13 23:52:02 -0800

    UGGGGGGHHHHH

--- PiezasTest.cpp
+++ PiezasTest.cpp
@@ -66,34 +66,34 @@ TEST(PiezasTest, testFullColumn){
 TEST(PiezasTest, testXWins){
 
   Piezas test;
-  // test.dropPiece(2);//X
-  // test.dropPiece(0);//O
-  // test.dropPiece(0);//X
-  // test.dropPiece(1);//O
-  // test.dropPiece(1);//X
-  // test.dropPiece(3);//O
-  // test.dropPiece(2);//X
-  // test.dropPiece(0);//O
-  // test.dropPiece(3);//X
-  // test.dropPiece(1);//O
-  // test.dropPiece(2);//X
-  // test.dropPiece(3);//O
+  test.dropPiece(2);//X
+  test.dropPiece(0);//O
+  test.dropPiece(0);//X
+  test.dropPiece(1);//O
+  test.dropPiece(1);//X
+  test.dropPiece(3);//O
+  test.dropPiece(2);//X
+  test.dropPiece(0);//O
+  test.dropPiece(3);//X
+  test.dropPiece(1);//O
+  test.dropPiece(2);//X
+  test.dropPiece(3);//O
 
-  test.dropPiece(3);// X
-  test.dropPiece(-2);// O
-  test.dropPiece(1);// X
-  test.dropPiece(0);// O
-  test.dropPiece(0);// X
-  test.dropPiece(1);// O
-  test.dropPiece(2);// X
-  test.dropPiece(3);// O
-  test.dropPiece(3);// X
-  test.dropPiece(2);// O
-  test.dropPiece(1);// X
-  test.dropPiece(-1);// O
-  test.dropPiece(0); //X
-  test.dropPiece(5); //o
-  test.dropPiece(2); //x wins horizontally
+  // test.dropPiece(3);// X
+  // test.dropPiece(-2);// O
+  // test.dropPiece(1);// X
+  // test.dropPiece(0);// O
+  // test.dropPiece(0);// X
+  // test.dropPiece(1);// O
+  // test.dropPiece(2);// X
+  // test.dropPiece(3);// O
+  // test.dropPiece(3);// X
+  // test.dropPiece(2);// O
+  // test.dropPiece(1);// X
+  // test.dropPiece(-1);// O
+  // test.dropPiece(0); //X
+  // test.dropPiece(5); //o
+  // test.dropPiece(2); //x wins horizontally
 
   Piece test_piece = test.gameState();
   ASSERT_EQ(test_piece, X);
@@ -124,15 +124,8 @@ TEST(PiezasTest, testGameNotOver){
   Piezas test;
   test.dropPiece(3);// X
   test.dropPiece(3);// O
-  // test.dropPiece(2);// X
-  // test.dropPiece(3);// O
-  // test.dropPiece(1);// X
-  // test.dropPiece(1);// O
-  // test.dropPiece(0);// X
-  // Piece test_piece = test.gameState();
-  // ASSERT_EQ(test_piece, Invalid);
+
   ASSERT_EQ(test.gameState(), Invalid);
-  //ASSERT_TRUE(Invalid);
 }
 
 
