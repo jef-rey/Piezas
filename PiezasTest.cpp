7a6f178f992647a6b9a8d372022cb3d5e7c80e20
Jeff Anderson <j3ff.anderson@gmail.com>
2019-12-13 21:15:44 -0800

    pushing above limit

--- PiezasTest.cpp
+++ PiezasTest.cpp
@@ -71,12 +71,13 @@ TEST(PiezasTest, testXWins){
   test.dropPiece(0);// O
   test.dropPiece(1);// X
   test.dropPiece(2);// O
+  test.dropPiece(2);// O
   test.dropPiece(2);// X
   test.dropPiece(1);// O
   test.dropPiece(3);// X
   test.dropPiece(0);// O
   Piece test_piece = test.gameState();
-  ASSERT_EQ(test_piece, Blank);
+  ASSERT_EQ(test_piece, X);
   //ASSERT_TRUE(X);
 }
 
