c0fc9d79b4f490e2d34f325219efba26fb85b61f
Jeff Anderson <j3ff.anderson@gmail.com>
2019-12-13 21:22:10 -0800

    lost faith in my ability and now i'm throwing knives

--- PiezasTest.cpp
+++ PiezasTest.cpp
@@ -64,13 +64,14 @@ TEST(PiezasTest, testFullColumn){
 TEST(PiezasTest, testXWins){
   Piezas test;
   test.dropPiece(3);// X
-  test.dropPiece(2);// O
-  test.dropPiece(0);// X
-  test.dropPiece(1);// O
-  test.dropPiece(3);// X
+  test.dropPiece(0);// O
+  test.dropPiece(2);// x
   test.dropPiece(0);// O
   test.dropPiece(1);// X
-  test.dropPiece(2);// O
+  test.dropPiece(3);// O
+  test.dropPiece(0);// X
+  test.dropPiece(1);// O
+  test.dropPiece(2);// X
   test.dropPiece(2);// O
   test.dropPiece(2);// X
   test.dropPiece(1);// O
