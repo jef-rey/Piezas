dcf567f9a42391345d3b215c853e01cdb087a1d3
Jeff Anderson <j3ff.anderson@gmail.com>
2019-12-13 23:33:04 -0800

    dunno

--- PiezasTest.cpp
+++ PiezasTest.cpp
@@ -66,18 +66,35 @@ TEST(PiezasTest, testFullColumn){
 TEST(PiezasTest, testXWins){
 
   Piezas test;
-  test.dropPiece(2);//X
-  test.dropPiece(0);//O
-  test.dropPiece(0);//X
-  test.dropPiece(1);//O
-  test.dropPiece(1);//X
-  test.dropPiece(3);//O
-  test.dropPiece(2);//X
-  test.dropPiece(0);//O
-  test.dropPiece(3);//X
-  test.dropPiece(1);//O
-  test.dropPiece(2);//X
-  test.dropPiece(3);//O
+  // test.dropPiece(2);//X
+  // test.dropPiece(0);//O
+  // test.dropPiece(0);//X
+  // test.dropPiece(1);//O
+  // test.dropPiece(1);//X
+  // test.dropPiece(3);//O
+  // test.dropPiece(2);//X
+  // test.dropPiece(0);//O
+  // test.dropPiece(3);//X
+  // test.dropPiece(1);//O
+  // test.dropPiece(2);//X
+  // test.dropPiece(3);//O
+
+  test.dropPiece(3);// X
+  test.dropPiece(-2);// O
+  test.dropPiece(1);// X
+  test.dropPiece(0);// O
+  test.dropPiece(0);// X
+  test.dropPiece(1);// O
+  test.dropPiece(2);// X
+  test.dropPiece(3);// O
+  test.dropPiece(3);// X
+  test.dropPiece(2);// O
+  test.dropPiece(1);// X
+  test.dropPiece(-1);// O
+  test.dropPiece(0); //X
+  test.dropPiece(5); //o
+  test.dropPiece(2); //x wins horizontally
+
   Piece test_piece = test.gameState();
   ASSERT_EQ(test_piece, X);
 }
